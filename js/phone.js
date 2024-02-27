08dc4c8338bf842c9366b8a3da68a46cb8460fff
pronoyrahman <ponoyrahman15@gmail.com>
2024-02-28 01:47:05 +0600

    2nd commit

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone=async()=>{
-    const res=await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone=async(searchText)=>{
+    const res=await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data=await res.json();
     const phones=data.data;
     // console.log(phone);
@@ -12,11 +12,15 @@ const displayPhones = phones => {
     //  console.log(phones);
     // step 1
     const phoneContainer = document.getElementById('phone-container');
+    //clear phone container card before adding new cards
+     phoneContainer.innerHTML ='';
+
+    //  proti ta phn ke niyar jnno forEach but alada kore kuthaw use hobe na
      phones.forEach(phone =>{
          console.log(phone)
         //  2 create div
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+        phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
         // step 3 set inner html
         phoneCard.innerHTML = `
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
@@ -34,5 +38,13 @@ const displayPhones = phones => {
      })
 }
 
+// handle search button
+const handleSearch = ()=>{
 
-loadPhone();
+    const searchField = document.getElementById('search-fields');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText)
+}
+
+// loadPhone();
